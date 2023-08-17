0973239850eb12aa86824057101b8838b570e591
wiltonfs <91587474+wiltonfs@users.noreply.github.com>
2023-08-17 13:16:35 -0700

    Dual-Plot CC Testing
    Added second plot to test script for CC plot testing

--- test.py
+++ test.py
@@ -4,14 +4,16 @@ import matplotlib.pyplot as plt
 x = [1, 2, 3, 4, 5]
 y = [10, 15, 7, 12, 9]
 
-# Create a figure and axis
-fig, ax = plt.subplots()
-ax.plot(x, y, label='Data')
-ax.set_xlabel('X-axis')
-ax.set_ylabel('Y-axis')
-ax.set_title('Simple Matplotlib Example')
-ax.legend()
+# Create a figure and save the figure to a specified path
+plt.figure(figsize=(8,8))
+plt.plot(x, y, label='Data')
+plt.title('Larger Matplotlib Example')
+plt.legend()
+plt.savefig('outputs/PlottingTestImageLarge.png')
 
-# Save the figure to a specified path
-save_path = 'outputs/PlottingTestImage.png'
-plt.savefig(save_path)
+# Create a second figure and save to the same path
+plt.figure(figsize=(2,2))
+plt.plot(y, x, label='Data')
+plt.title('Small Matplotlib Example')
+plt.legend()
+plt.savefig('outputs/PlottingTestImageSmall.png')
