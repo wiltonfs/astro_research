fb32c459f6db3bd9c2d5d3b229e868fa5b693e73
wiltonfs <91587474+wiltonfs@users.noreply.github.com>
2023-08-20 22:01:40 -0700

    Noise Grid Search

--- job-scripts/grid.sh
+++ job-scripts/grid.sh
@@ -9,11 +9,12 @@
 cd $PROJECT/astro_research
 
 # Define variables
-num_searches=30
+num_searches=50
 
 batch_range=(16 1024)
 lr_initial_range=(0.01 0.0001)
 lr_final_range=(0.001 0.00001)
+ns_range=(0.0 0.3)
 
 # Define function to generate random integer in a range
 function random_int() {
@@ -31,15 +32,23 @@ function random_float() {
 
 # Loop for num_searches iterations
 for ((i=1; i<=$num_searches; i++)); do
-  bs_value=$(random_int ${batch_range[0]} ${batch_range[1]})
-  lrI_value=$(random_float ${lr_initial_range[0]} ${lr_initial_range[1]})
-  lrF_value=$(random_float ${lr_final_range[0]} ${lr_final_range[1]})
+  ID="NoiseTune/Grid"
+  #bs=$(random_int ${batch_range[0]} ${batch_range[1]})
+  bs=32
+  #lrI=$(random_float ${lr_initial_range[0]} ${lr_initial_range[1]})
+  lrI=0.006
+  lrF=0.0005
+  I=100000
+  ns=$(random_float ${ns_range[0]} ${ns_range[1]})
+
   echo "Iteration $i:"
-  echo "bs_value = $bs_value"
-  echo "lrI_value = $lrI_value"
-  echo "lrF_value = $lrF_value"
-  echo "------"
-  sbatch job-scripts/sub-grid-script.sh $bs_value $lrI_value $lrF_value
+  echo "batch size = $bs"
+  echo "inital learning rate = $lrI"
+  echo "final learning rate = $lrF"
+  echo "iterations = $I"
+  echo "noise std = $ns"
+  echo "---------------"
+  sbatch job-scripts/sub-grid-script.sh $ID $bs $lrI $lrF $I $ns
 done
 
 
