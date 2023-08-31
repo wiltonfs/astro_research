07a38aa536b82e59cbf4ae9c4d1f6ea8108162e8
wiltonfs <91587474+wiltonfs@users.noreply.github.com>
2023-08-30 18:38:00 -0700

    MAPIE single label -> multi label
    Simple idea to try to use MAPIE on multiple labels

--- starnet-conformal-prediction.py
+++ starnet-conformal-prediction.py
@@ -2,39 +2,21 @@ import numpy as np
 from mapie.regression import MapieRegressor
 import matplotlib.pyplot as plt
 from utils.star_scikit import *
+from utils.star_datasets import *
 
+train_data_file = "data/synth_clean.h5"
+train_dataset = SimpleSpectraDataset(train_data_file, 'train')
+test_dataset = SimpleSpectraDataset(train_data_file, 'val')
 
+# MAPIE only handles single label regression:
+# Make mapie_regressor for each label
+# 
 
-model = StarNet()
+model = StarNetSciKit()
 mapie_regressor = MapieRegressor(model)
-mapie_regressor.fit(X_train, y_train)
+mapie_regressor.fit(train_dataset, None)
 
 # Predict with uncertainty using MAPIERegressor
-prediction, prediction_interval = mapie_regressor.predict(X_test, alpha=0.1)  # alpha determines the prediction level
+prediction, prediction_interval = mapie_regressor.predict(test_dataset, alpha=0.1)  # alpha determines the prediction level
 
-# Print predictions and prediction intervals
-for i in range(len(X_test)):
-    print(f"Input: {X_test[i][0]:.2f} | Prediction: {prediction[i]:.2f} | Prediction Interval: {prediction_interval[i].tolist()}")
-print("\n\n\n\n\n\n")
-# Calculate yerr:
-yerr = np.ones((2, len(X_test)))  # Initialize the yerr array with zeros
-print(yerr.shape)
-yerr[0, :] = prediction.squeeze() - prediction_interval[:, 0].squeeze()  # Calculate the lower errors
-yerr[1, :] = prediction_interval[:, 1].squeeze() - prediction.squeeze()  # Calculate the upper errors
-
-
-
-# Create a scatter plot with error bars
-plt.scatter(X_test, y_test, label="True Values", color='blue')
-#plt.scatter(X_test, prediction, label="Predictions", color='orange')
-plt.errorbar(X_test, prediction, yerr=yerr, fmt='o', label="Predictions", color='orange', ecolor='red')
-
-# Add labels and legend
-plt.xlabel("X")
-plt.ylabel("y")
-plt.title("Scatter Plot with Error Bars")
-plt.legend()
-
-# Show the plot
-plt.show()
 
--- utils/star_scikit.py
+++ utils/star_scikit.py
@@ -1,3 +1,65 @@
 # # Scikit Wrapper for starnet
 # Felix Wilton
 # 8/30/2023
+
+import torch
+from sklearn.base import BaseEstimator
+
+from utils.star_model import *
+
+class StarNetSciKit(BaseEstimator):
+    def __init__(self):
+        self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        self.model = None
+        
+    def fit(self, X, y, iters=1000, batch_size=16, initial_learning_rate=0.006, final_learning_rate=0.0005):
+        # Train your StarNet model on star_dataset for iters iterations
+        star_dataset = X
+        total_batch_iters = iters
+        cur_iter = 0
+
+        train_dataloader = torch.utils.data.DataLoader(star_dataset, batch_size=batch_size, shuffle=True, num_workers=0, pin_memory=True)
+        model = StarNet(self.device, star_dataset)
+        model = model.to(self.device)
+        optimizer = torch.optim.Adam(model.parameters(), initial_learning_rate, weight_decay=0)
+
+        # Calculate the learning rate schedule
+        lr_factor = (final_learning_rate / initial_learning_rate) ** (1.0 / total_batch_iters)
+        scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda epoch: lr_factor ** epoch)
+
+
+        # Continuously loop over the training set
+        while cur_iter < (total_batch_iters):
+            for train_batch in train_dataloader:
+                # Set parameters to trainable
+                model.train()
+                # Switch to GPU if available
+                train_batch = star_dataset.batch_to_device(train_batch, self.device)
+                # Zero the parameter gradients
+                optimizer.zero_grad()
+                # Forward propagation
+                label_preds = model(train_batch['spectrum'], norm_in=True, denorm_out=False)
+                # Compute mean-squared-error loss between predictions and normalized targets
+                loss = torch.nn.MSELoss()(label_preds, model.normalize(train_batch['labels'], model.labels_mean, model.labels_std))
+                # Back-propagation
+                loss.backward()
+                # Weight updates
+                optimizer.step()
+                scheduler.step()
+                cur_iter += 1
+
+        # Finished training
+        model.eval()
+        
+    def predict(self, X):
+        star_dataset = X
+        with torch.no_grad():
+            model_pred_labels = []
+            dataloader = torch.utils.data.DataLoader(star_dataset, batch_size=1024, shuffle=False, num_workers=0, pin_memory=True)
+            for batch in dataloader:
+                # Switch to GPU if available
+                batch = star_dataset.batch_to_device(batch, self.device)
+                # Forward propagation (and denormalize outputs)
+                label_preds = self.model(batch['spectrum'], norm_in=True, denorm_out=True)
+                # Save results
+                model_pred_labels.append(label_preds.cpu().data.numpy())
